77c6108f8fbed997d32348a79e38450035ccacf3
rizanurmaulana <rizanurmaulana2810@gmail.com>
2025-03-04 00:00:35 +0700

    initial project

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Homepage = () => {
+  return (
+    <section className='flex justify-center items-center min-h-screen'>
+      <h1 className='text-2xl font-bold tracking-tighter'>Pameraja.</h1>
+    </section>
+  );
+};
+
+export default Homepage;

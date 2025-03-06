0d5db51362391b2b9661cf052c04cb75c5ab169f
rizanurmaulana <rizanurmaulana2810@gmail.com>
2025-03-06 23:56:12 +0700

    feat: create login and register page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import { Route, Routes } from 'react-router-dom';
 import Homepage from './pages/Homepage';
+import Login from './pages/auth/Login';
+import Register from './pages/auth/Register';
 
 function App() {
   return (
     <Routes>
-      <Route index element={<Homepage />} />
+      <Route path='/' element={<Homepage />} />
+
+      <Route path='/auth/login' element={<Login />} />
+      <Route path='/auth/register' element={<Register />} />
     </Routes>
   );
 }
---
+++ src/pages/auth/Login.jsx
@@ -0,0 +1,64 @@
+import { Button } from '@/components/ui/button';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Login = () => {
+  return (
+    <section className='flex justify-center items-center min-h-screen'>
+      <Card className='w-[350px] shadow-none'>
+        <CardHeader className='items-center'>
+          <CardTitle className='text-2xl'>Login</CardTitle>
+          <CardDescription>Continue to your account.</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className='grid w-full items-center gap-4'>
+              <div className='flex flex-col space-y-1.5'>
+                <Label htmlFor='email'>Email</Label>
+                <Input
+                  id='email'
+                  type='email'
+                  name='email'
+                  placeholder='johndoe@mail.com'
+                />
+              </div>
+              <div className='flex flex-col space-y-1.5'>
+                <Label htmlFor='password'>Password</Label>
+                <Input
+                  id='password'
+                  type='password'
+                  name='password'
+                  placeholder='********'
+                />
+              </div>
+              <Button type='submit'>Login</Button>
+            </div>
+          </form>
+        </CardContent>
+        <CardFooter className='justify-center'>
+          <CardDescription>
+            Don't have an account?{' '}
+            <Link
+              className='text-neutral-950 font-medium hover:underline'
+              to='/auth/register'
+            >
+              Register
+            </Link>
+          </CardDescription>
+        </CardFooter>
+      </Card>
+    </section>
+  );
+};
+
+export default Login;
